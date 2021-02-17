4bdda0e9203df3c01a8b0a1a431ca8e142ff1eea
Ray.Lv <lugreen@gmail.com>
2021-02-17 23:44:21 +0800

    根据亮度自动退出

--- main.ts
+++ main.ts
@@ -1,14 +1,25 @@
 let 开始时间 = 0
+let 退处方式 = 0
 basic.forever(function () {
     basic.showNumber(input.lightLevel())
     if (input.lightLevel() <= 1) {
         开始时间 = input.runningTime()
+        退处方式 = 0
         while (input.runningTime() - 开始时间 <= 20000) {
-            music.playTone(input.runningTime() - 开始时间, music.beat(BeatFraction.Sixteenth))
-            basic.showIcon(IconNames.Butterfly)
-            basic.showIcon(IconNames.Chessboard)
+            if (input.lightLevel() <= 1) {
+                music.playTone(input.runningTime() - 开始时间, music.beat(BeatFraction.Sixteenth))
+                basic.showIcon(IconNames.Butterfly)
+                basic.showIcon(IconNames.Chessboard)
+            } else {
+                退处方式 = 1
+                break;
+            }
         }
-        basic.showIcon(IconNames.Yes)
-        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+        if (退处方式 == 0) {
+            basic.showIcon(IconNames.Yes)
+            music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+        } else {
+            basic.showIcon(IconNames.Asleep)
+        }
     }
 })
