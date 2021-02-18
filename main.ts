d149f0d254cfe48958a2b889d004f3312f202b7f
Ray.Lv <lugreen@gmail.com>
2021-02-18 23:37:02 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,26 @@
 let 开始时间 = 0
-let 退处方式 = 0
+let 退出方式 = 0
 basic.forever(function () {
-    basic.showNumber(input.lightLevel())
     if (input.lightLevel() <= 1) {
         开始时间 = input.runningTime()
-        退处方式 = 0
+        退出方式 = 0
         while (input.runningTime() - 开始时间 <= 20000) {
             if (input.lightLevel() <= 1) {
                 music.playTone(input.runningTime() - 开始时间, music.beat(BeatFraction.Sixteenth))
                 basic.showIcon(IconNames.Butterfly)
                 basic.showIcon(IconNames.Chessboard)
             } else {
-                退处方式 = 1
+                退出方式 = 1
                 break;
             }
         }
-        if (退处方式 == 0) {
+        if (退出方式 == 0) {
             basic.showIcon(IconNames.Yes)
             music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
         } else {
             basic.showIcon(IconNames.Asleep)
+            basic.pause(500)
+            basic.showNumber(input.runningTime() - 开始时间)
         }
     }
 })
